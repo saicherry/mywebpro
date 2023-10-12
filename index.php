da215bac87ef14f3fa95256cc4e0749f6af4aa4d
mkrishna <mkrishna@cure.com>
2023-10-12 18:40:41 +0530

    associative array & foreach loop

--- index.php
+++ index.php
@@ -9,3 +9,36 @@ $colors = ["red","green","blue"];
 echo $colors[2]."<br>";
 print_r ($colors);
 ?>
+
+
+<?php 
+// Associative Arrays
+$employees = [
+    "Sai" => 25000,
+    "Charan" => 45810,
+    "Kai" => 90000
+];
+echo "<br>";
+
+echo $employees["Sai"];
+echo "<br>";
+print_r ($employees);
+
+//foreach loop
+echo "<ol>";
+foreach($employees as $employee){
+    echo "<li>";
+    echo $employee."<br>";
+    echo "</li>";
+}
+echo "</ol>";
+
+echo "<ol>";
+foreach($employees as $key => $value){
+    echo "<li>$key = $value</li>";
+}
+echo "</ol>";
+
+
+
+?>
