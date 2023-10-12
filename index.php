f5fd88890fd926a17c7d93a3d71b2e58fc140221
mkrishna <mkrishna@cure.com>
2023-10-12 17:24:07 +0530

    arrays

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//Arrays
+
+$arrayItems = array(1,2,6,"dsfds","brr");
+echo $arrayItems[0]."<br>";
+print_r ($arrayItems);
+echo "<br>";
+$colors = ["red","green","blue"];
+echo $colors[2]."<br>";
+print_r ($colors);
+?>
